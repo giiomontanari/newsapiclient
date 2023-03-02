37908254550a96e710ece88887435bd6526294a3
Giovanni Montanari <giovannimontanari3@gmail.com>
2023-03-01 21:02:56 -0300

    add flow coroutines, create repository and create Resource

--- app/src/main/java/com/studies/newsapiclient/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/studies/newsapiclient/domain/repository/NewsRepository.kt
@@ -1,4 +1,16 @@
 package com.studies.newsapiclient.domain.repository
 
+import androidx.lifecycle.LiveData
+import com.studies.newsapiclient.data.model.ApiResponse
+import com.studies.newsapiclient.data.model.Article
+import com.studies.newsapiclient.data.util.Resource
+import kotlinx.coroutines.flow.Flow
+
 interface NewsRepository {
+    suspend fun getNewsHeadLines() : Resource<ApiResponse>
+    suspend fun getSearchedNews(searchQuery: String) : Resource<ApiResponse>
+    suspend fun saveNews(article: Article)
+    suspend fun deleteNews(article: Article)
+    fun getSavedNews() : Flow<List<Article>>
+
 }

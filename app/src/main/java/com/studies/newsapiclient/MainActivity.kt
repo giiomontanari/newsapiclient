316552270e968e7f6cdd5adfbe283be1573d6eb6
Giovanni Montanari <giovannimontanari3@gmail.com>
2023-03-08 21:06:04 -0300

    add navigation dependency

--- app/src/main/java/com/studies/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/studies/newsapiclient/MainActivity.kt
@@ -2,10 +2,14 @@ package com.studies.newsapiclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.studies.newsapiclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

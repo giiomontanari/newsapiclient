cb43ce340f54c7abe067be7d7670b3b34832a6b9
Giovanni Montanari <giovannimontanari3@gmail.com>
2023-03-14 21:37:44 -0300

    create navigation and modify newsviewmodelfactory

--- app/src/main/java/com/studies/newsapiclient/presentation/viewmodel/NewsViewModelFactory.kt
+++ app/src/main/java/com/studies/newsapiclient/presentation/viewmodel/NewsViewModelFactory.kt
@@ -10,7 +10,7 @@ class NewsViewModelFactory(
     private val getNewsHeadlinesUseCase: GetNewsHeadlinesUseCase
 ) : ViewModelProvider.Factory {
 
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return NewsViewModel(
             app,
             getNewsHeadlinesUseCase

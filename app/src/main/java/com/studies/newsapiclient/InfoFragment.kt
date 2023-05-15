865f57296a6d82005295f6daa94c8ee9da7d38ce
Giovanni Montanari <giovannimontanari3@gmail.com>
2023-05-15 18:27:57 -0300

    refactor rename infoFragment

--- app/src/main/java/com/studies/newsapiclient/InfoFragment.kt
+++ app/src/main/java/com/studies/newsapiclient/InfoFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [infoFragment.newInstance] factory method to
+ * Use the [InfoFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class infoFragment : Fragment() {
+class InfoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -49,7 +49,7 @@ class infoFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            infoFragment().apply {
+            InfoFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
